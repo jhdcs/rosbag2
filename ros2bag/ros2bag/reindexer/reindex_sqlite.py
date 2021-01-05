243c57778e7071f6cd4e4523d4afc4f306ef8e1b
Jacob Hassold <jhassold@dcscorp.com>
2021-01-05 11:07:47 -0500

    Reindexing file compression now supported Distro A, OPSEC #4584
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- ros2bag/ros2bag/reindexer/reindex_sqlite.py
+++ ros2bag/ros2bag/reindexer/reindex_sqlite.py
@@ -119,7 +119,10 @@ def reindex(
             print_error('Reindex needs a bag directory. Was given path "{}"'.format(uri)))
 
     # Get the relative paths
-    rel_file_paths = sorted(f for f in uri_dir.iterdir() if f.suffix == '.db3')
+    if compression_fmt == 'zstd':
+        rel_file_paths = sorted(f for f in uri_dir.iterdir() if f.suffix == '.zstd')
+    else:
+        rel_file_paths = sorted(f for f in uri_dir.iterdir() if f.suffix == '.db3')
 
     # Start recording metadata
     metadata = bag_metadata.MetadataWriter()
