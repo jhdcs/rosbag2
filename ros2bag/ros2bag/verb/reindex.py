103625b4902626e86ec8366dd7479ae857fba780
Jacob Hassold <jhassold@dcscorp.com>
2021-01-05 11:07:56 -0500

    Reverting to C++ version of reindexer
    Distro A, OPSEC #4584
    
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- ros2bag/ros2bag/verb/reindex.py
+++ ros2bag/ros2bag/verb/reindex.py
@@ -1,4 +1,4 @@
-# Copyright 2020 DCS Corporation, All Rights Reserved.
+# Copyright 2018 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,16 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# DISTRIBUTION A. Approved for public release; distribution unlimited.
-# OPSEC #4584.
-#
-# Delivered to the U.S. Government with Unlimited Rights, as defined in DFARS
-# Part 252.227-7013 or 7014 (Feb 2014).
-#
-# This notice must appear in all copies of this file and its derivatives.
 
-from ros2bag.api import check_path_exists, print_error
+import os
+
+from ros2bag.api import check_path_exists
 from ros2bag.verb import VerbExtension
 
 
@@ -31,7 +25,7 @@ class ReindexVerb(VerbExtension):
         parser.add_argument(
             'bag_file', type=check_path_exists, help='Bag file to reindex')
         parser.add_argument(
-            '-s', '--storage-id', default='sqlite3',
+            '-s', '--storage', default='sqlite3',
             help="storage identifier to be used, defaults to 'sqlite3'")
         parser.add_argument(
             '-f', '--serialization-format', default='',
