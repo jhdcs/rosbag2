bb439ed8a6914a54f2f49c3cb6e9548456c2084e
Jacob Hassold <jhassold@dcscorp.com>
2021-01-11 10:04:26 -0500

    First working version of script reindexer Distro A, OPSEC #4584
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- ros2bag/ros2bag/reindexer/reindex_sqlite.py
+++ ros2bag/ros2bag/reindexer/reindex_sqlite.py
@@ -23,14 +23,25 @@
 import pathlib
 import sqlite3
 import sys
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, TypedDict, Union 
 
 from ros2bag.api import print_error
 
-from . import bag_metadata
+import bag_metadata
 
+class TopicInfo(TypedDict):
+    topic_name: str
+    topic_type: str
+    topic_ser_fmt: str
+    topic_count: str
+    topic_qos: str
 
-def get_metadata(db_file: pathlib.Path) -> Dict[str, Union[List[Dict[str, Union[str, int]]], int]]:
+class DBMetadata(TypedDict):
+    topic_metadata: List[TopicInfo]
+    min_time: int
+    max_time: int
+
+def get_metadata(db_file: pathlib.Path) -> DBMetadata:
     db_con = sqlite3.connect(db_file)
     c = db_con.cursor()
 
@@ -41,17 +52,19 @@ def get_metadata(db_file: pathlib.Path) -> Dict[str, Union[List[Dict[str, Union[
               'GROUP BY topics.name;')
 
     # Set up initial values
-    topics: List[Dict[str, Union[str, int]]] = []
+    # topics: List[Dict[str, Union[str, int]]] = []
+    topics: List[TopicInfo] = []
     min_time: int = sys.maxsize
     max_time: int = 0
 
     # Aggregate metadata
     for row in c:
-        topics.append({'topic_name': row[0],
-                       'topic_type': row[1],
-                       'topic_ser_fmt': row[2],
-                       'message_count': row[3],
-                       'offered_qos_profiles': row[6]})
+        topics.append(TopicInfo(
+            topic_name= row[0],
+            topic_type=row[1],
+            topic_ser_fmt= row[2],
+            topic_count=row[3],
+            topic_qos=row[6]))
         if row[4] < min_time:
             min_time = row[4]
         if row[5] > max_time:
@@ -60,7 +73,7 @@ def get_metadata(db_file: pathlib.Path) -> Dict[str, Union[List[Dict[str, Union[
     return {'topic_metadata': topics, 'min_time': min_time, 'max_time': max_time}
 
 
-def reindex(uri: str, serialization_fmt: str, compression_fmt: str, compression_mode: str) -> None:
+def reindex(uri: str, compression_fmt: str, compression_mode: str, _test_output_dir: Optional[str]) -> None:
     """Reconstruct a metadata.yaml file for an sqlite3-based rosbag."""
     uri_dir = pathlib.Path(uri)
     if not uri_dir.is_dir():
@@ -74,6 +87,32 @@ def reindex(uri: str, serialization_fmt: str, compression_fmt: str, compression_
     metadata = bag_metadata.MetadataWriter()
     metadata.version = 4
     metadata.storage_identifier = 'sqlite3'
-    metadata.add_multiple_rel_file_paths(rel_file_paths)
+    metadata.add_multiple_rel_file_paths(p.relative_to(uri_dir) for p in rel_file_paths)
     metadata.compression_format = compression_fmt
     metadata.compression_mode = compression_mode
+
+    # Get topic info for each database
+    rolling_min_time = sys.maxsize
+    rolling_max_time = 0
+    for db_file in rel_file_paths:
+        db_metadata = get_metadata(db_file)
+        for topic in db_metadata['topic_metadata']:
+            metadata.add_topic(**topic)
+        
+        if rolling_min_time > db_metadata['min_time']:
+            rolling_min_time = db_metadata['min_time']
+        if rolling_max_time < db_metadata['max_time']:
+            rolling_max_time = db_metadata['max_time']
+    
+    metadata.starting_time = rolling_min_time
+    metadata.duration = rolling_max_time - rolling_min_time
+
+    if _test_output_dir is not None:
+        out_dir = pathlib.Path(_test_output_dir)
+        if not out_dir.is_dir():
+            raise ValueError(
+                print_error('Reindex test output needs a directory. Was given path "{}"'.format(uri)))
+    else:
+        out_dir = uri_dir
+
+    metadata.write_yaml(out_dir)
